7725da85d40f8e488b4cc7dd8f61f09ffafff6fa
Justin10-nesto <jastinlasway10@gmail.com>
2024-02-22 15:19:37 +0300

    python assigment one

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+print("Hello, {0}. You are {1} years old, and you live in {2}.".format(name, age, location))        
