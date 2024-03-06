7d105fc1117329d8fbdd2051f56a5c3794ab88fd
Justin10-nesto <jastinlasway10@gmail.com>
2024-03-06 17:54:17 +0300

    assigment number 2

---
+++ Week 2/assigment1.py
@@ -0,0 +1,27 @@
+#  question number 1
+my_list = []
+
+#question number 2
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+
+#question number 3
+my_list[2] = 15
+
+# question 4
+anather_list =[50, 60, 70]
+my_list.extend(anather_list)
+
+#question 5
+my_list.pop(0)
+
+#question 6
+my_list.sort()
+
+#question 7
+location = my_list.index(30)
+print("the location of 30 is :", location)
